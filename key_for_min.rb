57abb02e08aca7825bd83181c4c7aa18f64c2892
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 16:57:47 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  min_key = nil
+  min_value = nil
+  name_hash.each do |k, v|
+    if min_value == nil || v < min_value
+      min_value = v
+      min_key = k
+    end
+  end
+  min_key
 end

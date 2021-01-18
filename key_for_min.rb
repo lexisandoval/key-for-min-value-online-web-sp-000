41b7cd8cd81d70f14abffa80c776ea252909dafe
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 16:57:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |k, v|
-    if min_value == nil || min_value > v
+    if min_value == nil || v < min_value
       min_value = v
       min_key = k
     end

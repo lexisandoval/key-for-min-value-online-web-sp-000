e245a55f0c4987470b4c75895d2913c34cf1681c
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 16:53:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minimum = ""
-
   if name_hash.length == 0
     return nil
   end
+
+  minimum = ""
 
   name_hash.each do |key, value|
     if value < temp1
